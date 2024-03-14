fc268feae93dd039237215652b3f668e18fb8045
Leticia de la Osa <deletidev@gmail.com>
2024-03-14 17:47:06 +0100

    update game-catalog 01-displaying-data

--- demos/01 Displaying Data/game-catalog/src/app/app.component.ts
+++ demos/01 Displaying Data/game-catalog/src/app/app.component.ts
@@ -1,26 +1,28 @@
 import { Component } from '@angular/core';
-import { Game } from './model/game.model';
+import { RouterOutlet } from '@angular/router';
+import { Game } from './models/game.model';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'game-catalog';
   private games: Game[];
-  game: Game = new Game("");
+  game: Game = new Game('');
 
   constructor() {
-    console.log('** Constructor called **');
     this.games = [
       new Game('Super Mario Bros', '13 September 1985'),
       new Game('Legend of Zelda', '21 February 1986'),
       new Game('Sonic', '26 June 1981'),
     ];
+    console.log('** Constructor called **');
   }
-
-  ngOnInit(): void {
+  ngOnInit() {
     console.log('** ngOnInit called **');
     this.game = this.games[0];
   }
